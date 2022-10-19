36fc1db47a06e471c811874ce4b740d325b3e4b1
Isaac Meyer <icmeyer@mit.edu>
2022-10-18 21:22:57 -0400

    added nuclides to fresh bwr

--- problems/partial_bwr/fresh/input.py
+++ problems/partial_bwr/fresh/input.py
@@ -4,15 +4,20 @@ import sens_helpers
 
 nmesh = 5
 adj_nbatches = 100
-adj_nparticles = 100
+adj_nparticles = 10000
 clutch_nbatches = 100
-clutch_nparticles = 100
+clutch_nparticles = 10000
 # Sensitivities will be evaluated w.r.t. multipole parameters as well
 # as across the energy bins defined by sens_e_grid by default
 rxns = ['fission', 'absorption', 'elastic']
-nuclides = ['U235', 
-            'Gd154',
-            'Gd157']
+nuclides = ['H1', 'H2',
+            'O16', 'O17', 
+            'Fe54', 'Fe56', 'Fe57', 'Fe58',
+            'Cr50', 'Cr52', 'Cr53', 'Cr54',
+            'Ni58', 'Ni60', 'Ni61', 'Ni62', 'Ni64', 
+            'Zr90', 'Zr91', 'Zr92', 'Zr94', 'Zr96',
+            'Gd152','Gd154', 'Gd155', 'Gd156', 'Gd157', 'Gd158', 'Gd160',
+            'U234', 'U235', 'U236', 'U238']
 sens_e_grid = sens_helpers.GROUPS['SCALE-252'][::-1]
 # Clean xml directory, include '/' at end of directory string
 xml_dir = 'clean/'
